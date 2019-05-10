fdf04b0580dd7247bf68b89184dcf586a53fc011
OpusNocturn <gabriel.pimentel.sotc@gmail.com>
2019-05-10 13:15:17 -0300

    cadastro html transformado em arquivo php, css da pagina cargas melhorado

--- files/cargas.php
+++ files/cargas.php
@@ -2,24 +2,107 @@
     <head>
         <title> Cargas </title>
         <meta charset="UTF-8">
-        <link rel="stylesheet" type="text/css" href="login.css">
+        <!--<link rel="stylesheet" type="text/css" href="login.css">        -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <style>
+
+            body{
+                background: url(https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260) no-repeat;
+            }
+
+            ul {
+              list-style-type: none;
+              margin: 0;
+              padding: 0;
+              overflow: hidden;
+              background-color: #371f42;
+            }
+
+            li {
+              float: left;
+              border-right:1px solid #bbb;
+            }
+
+            li:last-child {
+              border-right: none;
+            }
+
+            li a {
+              display: block;
+              color: white;
+              text-align: center;
+              padding: 14px 16px;
+              text-decoration: none;
+            }
+
+            li a:hover:not(.active) {
+              background-color: #111; /*#111*/
+            }
+
+            .active {
+              background-color: #59c5e0; /*4CAf50*/
+            }
+
+            table, th, td {
+              border: 1px solid black;
+              border-collapse: collapse;
+              background: white;
+            }
+
+            th, td {
+              padding: 15px;
+              text-align: left;
+            }
+
+            footer{
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            background-color: black;
+            color: white;
+            text-align: center;
+            }
+
+        </style>
+
     </head>
 
     <body>
         <?php require "carga-class.php" ?>
+        <ul>
+          <li><a class="active" href="#home">Home</a></li>
+          <li><a href="#sobrenos">Sobre nós</a></li>
+          <li style="float:right"><a href="logout.php" class=a-menu-links>Logout</a></li>
+        </ul>
+
+
+       <div class="w3-sidebar w3-light-grey w3-bar-block" style="width:25%">
+          <h3 class="w3-bar-item">Menu</h3>
+          <a href="#vicargas" class="w3-bar-item w3-button">Visualizar cargas</a></li>
+          <a href="#cadcargas" class="w3-bar-item w3-button">Cadastrar cargas</a></li>
+          <a href="#viusuarios" class="w3-bar-item w3-button">Visualizar usuários</a></li>
+          <a href="cadastro.php" class="w3-bar-item w3-button">Cadastrar usuários</a></li>
+        </div>
+
+        <div style="margin-left:25%">
 
         <?php
 
+         
+
         session_start();    # captura os dados da sessão atual
         if(isset($_SESSION['isLogged']) && $_SESSION['isLogged'] == true){
-            echo "<a href=\"logout.php\" class=\"a-menus-links\"> logout</a>";
+            #echo "<a href=\"logout.php\" class=\"a-menus-links\"> logout</a>";
+            echo "<br><br><br>";
             # simulação do banco de dados
             $cargas = array();
             $cargas[0] = new Carga("1", "04/05/2019", "19/05/2019", "carga 1");
             $cargas[1] = new Carga("2", "04/05/2019", "25/07/2019", "carga 2");
             $cargas[2] = new Carga("3", "04/05/2019", "05/06/2019", "carga 3");
 
-            echo "<table>";    # inicio do bloco de tabelas
+            echo "<table align=center>";    # inicio do bloco de tabelas
 
             echo "<tr> 
                     <th>ID</th>
@@ -50,5 +133,12 @@
             echo "<meta http-equiv=\"refresh\" content=\".4; url=login.php\"/> ";
         }
         ?>
+
+        <footer>
+        <div id='contact'>
+                <p><strong>email:</strong> seutransporte.com | <strong>telefone:</strong> 917-555-1098 | Todos os direitos reservados ©</p></div>
+        </footer>
+
+
     </body>
 </html>
